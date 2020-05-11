29addcaeec6bceb174c8c4fc5e91df40290710b8
vinsbld <vincent.blndel@gmail.com>
2020-05-11 15:14:21 +0200

    clean code

--- myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
+++ myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptableTest.java
@@ -27,15 +27,16 @@ public class EcritureComptableTest {
     public void initEcritureComptable(){
         vEcriture = new EcritureComptable();
 
+    }
+    
+    @Test
+    public void isEquilibree() {
+
         vEcriture.getListLigneEcriture().add(this.createLigne(1, "200.50", null));
         vEcriture.getListLigneEcriture().add(this.createLigne(1, "100.50", "33"));
         vEcriture.getListLigneEcriture().add(this.createLigne(2, null, "301"));
         vEcriture.getListLigneEcriture().add(this.createLigne(2, "40", "7"));
-    }
 
-
-    @Test
-    public void isEquilibree() {
         assertThat(vEcriture.isEquilibree()).isEqualTo(true);
     }
 
