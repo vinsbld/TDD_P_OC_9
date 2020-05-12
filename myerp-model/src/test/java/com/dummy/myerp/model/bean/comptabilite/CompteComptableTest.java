0dbe0ae1bcd2adb8657df31550e3848215f6f605
vinsbld <vincent.blndel@gmail.com>
2020-05-12 17:59:06 +0200

    test CompteComptable

--- myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/CompteComptableTest.java
+++ myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/CompteComptableTest.java
@@ -1,6 +1,37 @@
 package com.dummy.myerp.model.bean.comptabilite;
 
 
+import com.dummy.myerp.model.validation.constraint.MontantComptable;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CompteComptableTest {
+
+    private List<CompteComptable> compteComptableList;
+
+    @Before
+    public void initCompteComptable(){
+        compteComptableList = new ArrayList<CompteComptable>();
+        compteComptableList.add(new CompteComptable(1234,"Compte courant 1"));
+        compteComptableList.add(new CompteComptable(123456, "Compte courant 2"));
+
+    }
+
+    @Test
+    public void getByNumero_whenComptComptableExist(){
+
+        assertThat(CompteComptable.getByNumero(compteComptableList, 1234).getLibelle()).isEqualTo("Compte courant 1");
+    }
+
+    @Test
+    public void getByNumero_whenCompteComptableNotExist(){
+
+        assertThat(CompteComptable.getByNumero(compteComptableList, 12)).isEqualTo(null);
+
+    }
 }
