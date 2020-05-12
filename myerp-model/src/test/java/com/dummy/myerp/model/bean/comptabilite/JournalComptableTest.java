d144764abccdbf9d2650dd96e017a461602668e5
vinsbld <vincent.blndel@gmail.com>
2020-05-12 18:13:52 +0200

    add initJournalComptable

--- myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/JournalComptableTest.java
+++ myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/JournalComptableTest.java
@@ -1,4 +1,18 @@
 package com.dummy.myerp.model.bean.comptabilite;
 
+import org.junit.Before;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JournalComptableTest {
+
+    private List<JournalComptable> journalComptables;
+
+    @Before
+    public void initJournalComptable(){
+        journalComptables = new ArrayList<JournalComptable>();
+        journalComptables.add(new JournalComptable("ACH","Achat"));
+
+    }
 }
